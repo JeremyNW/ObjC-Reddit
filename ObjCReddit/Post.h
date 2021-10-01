3b204dc4780b35a485b4df703ca8ef1ec7b7ae9b
JeremyNW <jeddynwarren@gmail.com>
2021-10-01 07:22:22 -0600

    Initial Commit

---
+++ ObjCReddit/Post.h
@@ -0,0 +1,25 @@
+//
+//  Post.h
+//  ObjCReddit
+//
+//  Created by Jeremy Warren on 10/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Post : NSObject
+
+@property NSString *title;
+@property NSString *subreddit;
+@property NSInteger upvotes;
+@property NSInteger comments;
+
+- (instancetype) initWithTitle:(NSString *)title
+                       upvotes:(NSInteger)upvotes
+                      comments:(NSInteger)comments;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjCReddit/Post.m
@@ -0,0 +1,12 @@
+//
+//  Post.m
+//  ObjCReddit
+//
+//  Created by Jeremy Warren on 10/1/21.
+//
+
+#import "Post.h"
+
+@implementation Post
+
+@end
